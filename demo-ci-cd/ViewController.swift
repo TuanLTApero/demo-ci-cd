cef833c58328cb972c450817783914fed5400f16
TuanLT <tuanlt@apero.vn>
2023-10-03 11:32:07 +0700

    first commit

---
+++ demo-ci-cd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo-ci-cd
+//
+//  Created by Le Tuan on 03/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("AAAA")
+    }
+
+
+}
+
